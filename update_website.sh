92f719dcbd98185432b7c6efb4cd3cd6bb8e6a0e
dannielshalev <danniel.shalev@kenshoo.com>
2021-06-08 16:48:19 +0300

    init

---
+++ update_website.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "<!doctype html>
+<html>
+  <head>
+    <title>This is danniel exam webpage</title>
+  </head>
+  <body>
+    <p> Hello visitor! <strong>the build number is: $1 !</strong> </p>
+  </body>
+</html>" > index.html
